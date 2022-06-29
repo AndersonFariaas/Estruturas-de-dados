1fcea93c3dcb1f3efa47b0f532c17e2d3369ac7f
Anderson Farias <anderson.bfc@hotmail.com>
2022-06-29 14:32:21 -0300

    First Commit

---
+++ script.js
@@ -0,0 +1,35 @@
+// ------ 01 ------ //
+
+let familia = ['Anderson', 'Allan', 'Lais', 'Ellen', 'Adelson', 'Leide'];
+
+// ------ 02 ----- //
+
+familia.unshift('Sansão');
+
+console.log(familia);
+
+// ------ 03 ---- //
+
+familia.pop();
+
+console.log(familia);
+
+// ----- 04 ----- //
+
+familia.push('Rubia', 'Maria');
+
+console.log(familia);
+
+// ------ 05 ----- //
+
+familia.shift();
+
+console.log(familia);
+
+// ---- 06 ---- //
+
+let numbers = [7, 5, 6, 0, 2, 4, 1, 3, 9, 8];
+
+numbers.sort();
+
+console.log(numbers);
